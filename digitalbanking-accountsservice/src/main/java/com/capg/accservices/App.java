b8574ccea892fefa6b4c52993f95ab733f372536
spansare <spansari@in.ibm.com>
2016-12-07 13:54:07 +0530

    Update App.java

--- digitalbanking-accountsservice/src/main/java/com/capg/accservices/App.java
+++ digitalbanking-accountsservice/src/main/java/com/capg/accservices/App.java
@@ -6,8 +6,8 @@ package com.capg.accservices;
  */
 public class App 
 {
-    public static void main( String[] args )
+    /*public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+    }*/
 }
