5cf50a60a3c35383aca5d9c104347befd8959448
spansare <spansari@in.ibm.com>
2016-12-15 18:38:54 +0530

    Update settings.py

--- digitalbanking-transferservice/some_bank/some_bank/settings.py
+++ digitalbanking-transferservice/some_bank/some_bank/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'some_bank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': (os.path.join(BASE_DIR, 'templates')),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
